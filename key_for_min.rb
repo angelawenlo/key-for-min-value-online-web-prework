70717d4452fd50ad5136cb0ca68581ad387c5a69
Angela Lo <angelawenlo@gmail.com>
2019-05-17 17:20:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value (hash)
   hash.map do |key, value|
-    
+    if value.min 
+      return key
+    else
+    end
   end
 end
